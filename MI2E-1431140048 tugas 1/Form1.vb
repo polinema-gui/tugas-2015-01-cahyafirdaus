13cbc839a2af4c2d96f140fc24d4f1f5b10dbda5
Cahya Firdaus Putra <cahya.firdaus.putra@gmail.com>
2015-09-30 17:13:27 +0700

    ini tugas pertemuan 2

---
+++ MI2E-1431140048 tugas 1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.Red Then
+            MyBase.BackColor = Color.Blue
+        Else : MyBase.BackColor = Color.Red
+        End If
+
+    End Sub
+End Class
